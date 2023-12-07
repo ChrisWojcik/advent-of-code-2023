e18c8fd675aeafb33c9bd200501aca10a178cd0b
Christopher Wojcik <cpw1485@gmail.com>
2023-12-06 23:01:24 -0500

    day 2

---
+++ 02/1.py
@@ -0,0 +1,34 @@
+import sys
+
+max_per_color = {
+  'red': 12,
+  'green': 13,
+  'blue': 14
+}
+
+sum = 0
+game_id = 1
+
+for line in sys.stdin:
+  line = line.strip()
+
+  [game_name, rounds] = line.split(': ')
+  
+  rounds = rounds.split('; ')
+  rounds = [round.split(', ') for round in rounds]
+  rounds = [choice.split(' ') for round in rounds for choice in round]
+  rounds = [(int(round[0]), round[1]) for round in rounds]
+
+  game_possible = True
+
+  for [number, color] in rounds:
+    if number > max_per_color[color]:
+      game_possible = False
+      break
+
+  if game_possible:
+    sum += game_id
+
+  game_id += 1
+
+print(sum)
---
+++ 02/2.py
@@ -0,0 +1,28 @@
+import sys
+import math
+
+sum = 0
+
+for line in sys.stdin:
+  line = line.strip()
+
+  [game_name, rounds] = line.split(': ')
+  
+  rounds = rounds.split('; ')
+  rounds = [round.split(', ') for round in rounds]
+  rounds = [choice.split(' ') for round in rounds for choice in round]
+  rounds = [(int(round[0]), round[1]) for round in rounds]
+
+  fewest_per_color = {
+    'red': 0,
+    'green': 0,
+    'blue': 0
+  }
+
+  for [number, color] in rounds:
+    fewest_per_color[color] = max(fewest_per_color[color], number)
+
+  power_of_set = fewest_per_color['red'] * fewest_per_color['green'] * fewest_per_color['blue']
+  sum += power_of_set
+
+print(sum)
